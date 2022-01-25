18839daf7c025610f9e4e881e010ac606a65dd6d
Prova Digital <69012272+provadigital@users.noreply.github.com>
2022-01-25 11:34:07 -0300

    Create another_dup.ts

---
+++ another_dup.ts
@@ -0,0 +1,26 @@
+class Student {
+	fullName: string;
+	constructor(
+		public firstName: string,
+		public middleInicial: string,
+		public lastName: string
+	){
+		this.fullName = firstName + " " + middleInicial + " " + lastName;
+	}
+}
+
+interface Person {
+	firstName: string;
+	lastName: string;
+}
+
+function greeter(person: Person) {
+	return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = new Student("Charles", "H.", "Santos");
+let password = "it should't be here"
+var password = "it should't be here"
+let token = "just to trigger a build"
+
+document.body.textContent = greeter(user);;
